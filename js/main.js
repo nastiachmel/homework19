67d724e03b5332d9ae0468008e905606cf038e0f
nastiachmel <nastiachmel@gmail.com>
2019-11-22 14:36:25 +0200

    add

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+console.log('sdfs');
+
+$(function () {
+  //ie detection
+  if ($.browser.msie && $.browser.version == 10) {
+      $("html").addClass("ie10");
+  }
+  if ($.browser.msie && $.browser.version == 9) {
+      $("html").addClass("ie9");
+  }
+  if ($.browser.msie && $.browser.version == 8) {
+      $("html").addClass("ie8");
+  }
+});
+
+
